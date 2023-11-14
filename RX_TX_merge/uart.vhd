389c0f80155293958f23d30ac05e4b927c0b808a
alvargu <92863607+alvargu@users.noreply.github.com>
2023-11-14 16:04:08 +0100

    changed predefined char to ascii for 9

--- RX_TX_merge/uart.vhd
+++ RX_TX_merge/uart.vhd
@@ -8,7 +8,7 @@ entity uart is
 		constant baud_rate: integer := 9600;
 		constant time_led_on: integer := 50; /* 50 ms */
 		constant o_smp_bits: integer := 8;
-          constant predefined_char : std_logic_vector(7 downto 0) := "00000000"
+          constant predefined_char : std_logic_vector(7 downto 0) := "00111001"
 		);
 	port (
 		clk: in std_logic;
